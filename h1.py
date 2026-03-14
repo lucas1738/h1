608c8a9505df32e679645d757f7b0c51130d401f
lucas <tumwinepiuslucas1@gmail.com>
2026-03-13 18:19:13 -0700

    new

---
+++ h1.py
@@ -0,0 +1,11 @@
+#print(10/0)
+try:  
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    result = num1 / num2
+except ZeroDivisionError:
+    print("you can't divide zero")
+else:
+    print(f"the answer is {result}")
+    
+
